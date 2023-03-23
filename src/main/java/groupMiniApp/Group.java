1c0423345fa630c6dfda03e61f2fe46a294d6120
YanivLT <YanivLT@DESKTOP-GBU789F>
2023-03-23 16:44:02 +0200

    Update

--- src/main/java/groupMiniApp/Group.java
+++ src/main/java/groupMiniApp/Group.java
@@ -1,6 +1,8 @@
-package demo;
+package groupMiniApp;
 
 import java.util.ArrayList;
+
+import demo.User;
 
 public class Group {
 	
--- src/main/java/groupMiniApp/GroupController.java
+++ src/main/java/groupMiniApp/GroupController.java
@@ -1,4 +1,4 @@
-package demo;
+package groupMiniApp;
 
 import java.util.HashMap;
 import java.util.Map;
