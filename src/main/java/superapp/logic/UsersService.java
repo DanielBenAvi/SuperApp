48426ee18f5b65de090b59bb43f283fc3d67bb42
daniel ben avi <ben.avi.daniel@s.afeka.ac.il>
2023-04-02 15:15:11 +0300

    added user service interface

--- src/main/java/superapp/logic/UsersService.java
+++ src/main/java/superapp/logic/UsersService.java
@@ -1,4 +1,18 @@
 package superapp.logic;
 
+import superapp.logic.boundaries.UserBoundary;
+
+import java.util.List;
+
 public interface UsersService {
+    public UserBoundary createUser(UserBoundary userBoundary);
+
+    public UserBoundary login(String userSuperApp, String userEmail);
+
+    public UserBoundary updateUser(String userSuperApp, String userEmail, UserBoundary update);
+
+    public List<UserBoundary> getAllUsers();
+
+    public void deleteAllUsers();
+
 }
