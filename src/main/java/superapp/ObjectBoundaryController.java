32204a7d193a4de55d4c6d8af0472e61d662c8c0
YosefOwl <93843185+YosefOwl@users.noreply.github.com>
2023-04-02 16:09:32 +0300

    implement object service interface on object controller

--- src/main/java/superapp/ObjectBoundaryController.java
+++ src/main/java/superapp/ObjectBoundaryController.java
@@ -1,53 +1,34 @@
 package superapp;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
-import superapp.logic.boundaries.CreatedBy;
+import org.springframework.web.bind.annotation.*;
+import superapp.logic.ObjectsService;
 import superapp.logic.boundaries.SuperAppObjectBoundary;
-import superapp.logic.boundaries.ObjectId;
-import superapp.logic.boundaries.UserID;
+
 
 @RestController
 public class ObjectBoundaryController {
-	
+
+	private ObjectsService objectsService;
+
+	@Autowired
+	public void setObjectsService(ObjectsService objectsService) {
+		this.objectsService = objectsService;
+	}
+
 	/**
 	 * Request specific Object Boundary
 	 * @return Object Boundary
-	 * 
 	 * @author Daniel & Yaniv
 	 */
-	@RequestMapping(
-			path = {"/superapp/objects/{superapp}/{internalObjectId}"},
-			method = {RequestMethod.GET},
-			produces = {MediaType.APPLICATION_JSON_VALUE})
-	public SuperAppObjectBoundary retriveObject(
-			@PathVariable("superapp") String superapp,
-			@PathVariable("internalObjectId") String internalObjectId){
-		SuperAppObjectBoundary ob = new SuperAppObjectBoundary();
-		// hardCode
-		ob.setObjectId(new ObjectId());
-		ob.getObjectId().setInternalObjectId(internalObjectId);
-		ob.getObjectId().setSuperApp(superapp);
-		ob.setType("dummyType");
-		ob.setAlias("demo instance");
-		ob.setActive(true);
-		ob.setCreateTimeStamp(new Date());
-		UserID ud = new UserID("jill@demo.org",superapp);
-		CreatedBy cb = new CreatedBy();
-		cb.setUserId(ud);
-		ob.setCreatedBy(cb);
-		ob.setObjectDetails(new Date());
-		// hardCode
-		return ob;
+	@GetMapping(path = {"/superapp/objects/{superapp}/{internalObjectId}"},
+				produces = {MediaType.APPLICATION_JSON_VALUE})
+	public SuperAppObjectBoundary getObject(@PathVariable("superapp") String superapp,
+											@PathVariable("internalObjectId") String internalObjectId) {
+
+		return objectsService.getSpecificObject(superapp, internalObjectId);
 	}
 	
 	/**
@@ -56,37 +37,28 @@ public class ObjectBoundaryController {
 	 * 
 	 * @author Daniel & Yaniv
 	 */
-	@RequestMapping(
-			path = {"/superapp/objects"},
-			method = {RequestMethod.GET},
-			produces = {MediaType.APPLICATION_JSON_VALUE})
-	public Map<String, SuperAppObjectBoundary> getAllObjects(){
-		Map<String, SuperAppObjectBoundary> objectBoundaryHash = new HashMap<>();
-//		hard code
-		objectBoundaryHash.put("1", new SuperAppObjectBoundary());
-		objectBoundaryHash.put("2", new SuperAppObjectBoundary());
-		
-		return objectBoundaryHash;
+	@GetMapping(path = {"/superapp/objects"},
+				produces = {MediaType.APPLICATION_JSON_VALUE})
+	public SuperAppObjectBoundary[] getAllObjects() {
+
+		return this.objectsService.getAllObjects()
+				.toArray(new SuperAppObjectBoundary[0]);
 	}
 	
 	/**
 	 * Create new Object
 	 * @return Object Boundary
 	 * @param superAppObjectBoundary as ObjectBoundary
-	 * 
 	 * @author Daniel & Yaniv
 	 */
-	@RequestMapping(
+	@PostMapping(
 			path = {"/superapp/objects"},
-			method = {RequestMethod.POST},
 			produces = {MediaType.APPLICATION_JSON_VALUE},
 			consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public SuperAppObjectBoundary createObject(@RequestBody SuperAppObjectBoundary superAppObjectBoundary) {
-//		hard code
-		superAppObjectBoundary.setObjectId(new ObjectId());
-		superAppObjectBoundary.getObjectId().setSuperApp("2023b.liorAriely");
-		System.err.println(superAppObjectBoundary.getObjectId().getInternalObjectId());
-		return superAppObjectBoundary;
+
+		// TODO : question about path (user ID), Ido & Yosef
+		return this.objectsService.createObject(superAppObjectBoundary);
 	}
 	
 	
@@ -96,23 +68,22 @@ public class ObjectBoundaryController {
 	 * @param internalObjectId the object id
 	 * @param updatedObject
 	 */
-	@RequestMapping(
+	@PutMapping(
 			path = {"/superapp/objects/{superapp}/{internalObjectId}"},
-			method = {RequestMethod.PUT},
 			consumes = {MediaType.APPLICATION_JSON_VALUE})
-	public void updateObject(
-			@PathVariable("superapp") String superapp,
-			@PathVariable("internalObjectId") String internalObjectId,
-			@RequestBody SuperAppObjectBoundary updatedObject) {
-		
-		if (updatedObject.getObjectId() == null || 
-				updatedObject.getObjectId().getInternalObjectId() == null) {
-			System.err.println("ObjectId or internalObjectId is null");
-		}
-		else {
-		System.err.println("Updating object #" + internalObjectId + " using: " + updatedObject);
-		}
-		
+	public void updateObject(@PathVariable("superapp") String superapp,
+							 @PathVariable("internalObjectId") String internalObjectId,
+							 @RequestBody SuperAppObjectBoundary updatedObject) {
+
+		this.objectsService.updateObject(superapp, internalObjectId, updatedObject);
 	}
-	
+
+
+	// just for testing
+//	@DeleteMapping(path = {"/superapp/admin/objects"})
+//	public void deleteAllObjects() {
+//		// do nothing
+//		this.objectsService.deleteAllObjects();
+//		System.err.println("all objects deleted ");
+//	}
 }
