fccafbb51a2edf2b62109fdd5d64ec2605a19931
daniel ben avi <ben.avi.daniel@s.afeka.ac.il>
2023-04-14 18:19:32 +0300

    delete old code

--- src/main/java/superapp/UserController.java
+++ src/main/java/superapp/UserController.java
@@ -32,8 +32,7 @@ public class UserController {
 	@GetMapping(path = {"/superapp/users/login/{superapp}/{email}"},
 			produces = {MediaType.APPLICATION_JSON_VALUE})
 	public UserBoundary login(@PathVariable("email") String email, @PathVariable("superapp") String superapp) {
-//		UserID userId = new UserID(superapp, email);
-//		return new UserBoundary(userId, "Student", "Dani", "avatar");
+
 		return this.usersService.login(superapp,email).orElseThrow(() -> new RuntimeException("Could not login: "+superapp+" "+email));
 	}
 
@@ -47,7 +46,7 @@ public class UserController {
 			produces = {MediaType.APPLICATION_JSON_VALUE},
 			consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public UserBoundary createUser(@RequestBody NewUserBoundary newUser) {
-
+		// TODO: create function that convert NewUserBoundary to UserBoundary in usersService
 
 		// convert NewUserBoundary to UserBoundary
 		UserBoundary userBoundary= new UserBoundary();
@@ -59,7 +58,6 @@ public class UserController {
 		userBoundary.setRole(newUser.getRole());
 
 		return this.usersService.createUser(userBoundary);
-//		return new UserBoundary(userId, newUser.getRole(),newUser.getUserName(),newUser.getAvatar());
 	}
 
 	/**
@@ -71,10 +69,6 @@ public class UserController {
 	@PutMapping(path = {"/superapp/users/{superapp}/{userEmail}"},
 			consumes = {MediaType.APPLICATION_JSON_VALUE})
 	public void updateUser(@PathVariable("userEmail") String userEmail, @PathVariable("superapp") String superapp,@RequestBody UserBoundary updatedUser) {
-
-//		System.err.println("update user UrlMail: " + userEmail);
-//		System.err.println("update user UrlSuperapp: " + superapp);
-//		System.err.println("update user Info: " + updatedUser);
 
 		this.usersService.updateUser(userEmail,superapp,updatedUser);
 
